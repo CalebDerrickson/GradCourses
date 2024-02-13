d3116c6b922e6d21678432b40caf04f089eeaf79
Caleb Derrickson <Calebderrickson@gmail.com>
2024-02-13 14:58:17 -0600

    Working on NL Hw5

--- Quarter 2/Nonlinear Optimization/Homework/Homework5/MatlabCode/hwk5p4b.m
+++ Quarter 2/Nonlinear Optimization/Homework/Homework5/MatlabCode/hwk5p4b.m
@@ -12,11 +12,19 @@ for i = 1:max(size(BFGS_seq))
     Norm_seq(i) = norm(xstar- BFGS_seq(i, :));
 end
 
+q_seq = zeros(n-2, 1);
+
+for i = 2:max(size(BFGS_seq)) - 1
+    q_seq(i) = log(Norm_seq(i-1)/Norm_seq(i))/log(Norm_seq(i+1)/Norm_seq(i));
+end
 
 semilogy(Norm_seq, LineWidth=2);
 grid on;
 title("Superlinear Convergence for n=30");
 ylabel("$|| x_k - x^* ||$", Interpreter="latex");
 xlabel("Iterate $k$", Interpreter="latex");
+
+figure(2);
+plot(q_seq)
 
 end
